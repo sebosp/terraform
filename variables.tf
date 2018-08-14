0e1ebd1ac299dc54545ddd4dd8ccd0266f452639
Seb Ospina <kraige@gmail.com>
2018-08-14 18:31:40 +0000

    Adding keybase target for PGP encrypted secrets

--- variables.tf
+++ variables.tf
@@ -4,6 +4,9 @@ variable "env" {
 variable "google_project" {
   default = "sebosp-main-666"
 }
+variable "keybase_target" {
+  default = "badhorsie"
+}
 locals {
   common_tags = "${map(
     "Project", "sys",
